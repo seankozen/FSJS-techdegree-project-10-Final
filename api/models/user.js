8307300cec9660f7a4ffe6d55a8c7da353063c9e
Sean Kozen <sgkozen@gmail.com>
2022-02-20 11:37:27 +0900

    Create api

---
+++ api/models/user.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+
+module.exports = (sequelize) => {
+    class User extends Model {}
+    User.init({
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notNull: {
+                msg: 'A first name is required.'
+              },
+              notEmpty: {
+                msg: 'Please provide a first name.'   
+              },  
+            },
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notNull: {
+                msg: 'A last name is required.'
+              },
+              notEmpty: {
+                msg: 'Please provide a last name.'   
+              },  
+            },
+        },  
+        emailAddress: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+            unique: {
+              msg: 'The email you entered already exists'  
+            },
+            validate: {
+              notNull: {
+                msg: 'An email is required.'
+              },
+              isEmail: {
+                msg: 'Please provide a valid email address.'   
+              }    
+            },   
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            set(val) {
+              const hashedPassword = bcrypt.hashSync(val, 10);
+              this.setDataValue('password', hashedPassword);
+            },
+            validate: {
+              notNull: {
+                msg: 'A password is required'
+              },
+              notEmpty: {
+                msg: 'Please provide a password.'   
+              },   
+            },
+        },
+    }, { sequelize });
+
+    //One to many association to course
+    User.associate = (models) => {
+      User.hasMany(models.Course, {
+        foreignKey: {
+          fieldName: 'userId',
+          allowNull: false,
+        },
+      });
+    }
+
+    return User;
+
+};
