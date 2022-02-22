8855a705df510b3c6c4a43a599d10df97980ba7d
Sean Kozen <sgkozen@gmail.com>
2022-02-22 13:03:31 +0900

    Create Course working

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -9,56 +9,129 @@ function CreateCourse () {
     const history = useHistory();
     const location = useLocation();
     const authUser = context.authenticatedUser;
-    
 
-
-
-
+    // Return path after course creation
     const { from } = location.state || { from: { pathname: "/"} }
 
-
-
     // Component states
-    const[course, setCourse] = useState('');
-    
+    const[title, setTitle] = useState('');
+    const[description, setDescription] = useState('');
+    const[estimatedTime, setEstimatedTime] = useState('');
+    const[materialsNeeded, setMaterialsNeeded] = useState('');
+    const[errors, setErrors] = useState('');
+
+    // Function to set state on change    
+    const change = (event) => {
+        if (event.target.name === 'courseTitle') {
+            setTitle(event.target.value)
+        } else if (event.target.name === 'courseDescription') {
+            setDescription(event.target.value)
+        } else if (event.target.name === 'estimatedTime') {
+            setEstimatedTime(event.target.value)
+        } else if (event.target.name === 'materialsNeeded') {
+            setMaterialsNeeded(event.target.value)
+        } else {
+            return;
+        }
+    }
+
+    const submit = () => {
+
+        const course = {
+            title,
+            description,
+            estimatedTime,
+            materialsNeeded,
+            userId: authUser.id
+        }
+
+        // Post data to create course
+        context.data.createCourse(course, authUser.emailAddress, authUser.password)
+            .then(errors => {
+                if(errors.length) {
+                    setErrors(errors);
+                } else {
+                    console.log("Your course has been created.");
+                    history.push("/courses");
+                }
+            })
+            .catch ( err => {
+                console.error(errors);
+                history.push('/error');  // Push to history stack
+            });
+
+
+    }
+
+
+
+
+
+
+
+    // Cancel button
+    const cancel = () => {
+        history.push(from);
+    }
+
 
     
-        return (
+    return ( 
+        <div className="wrap">
+            <h2>Create Course</h2>
+            <Form
+                submitButtonText = "Create Course"
+                submit = {submit}
+                cancel = {cancel}
+                errors = {errors}
 
-            
-            <div className="wrap">
-                <h2>Create Course</h2>
-                <Form
-                    submitButtonText = "Create Course"
-                    //submit = {this.submit}
-                    //cancel = {this.cancel}
-                    //errors = {errors}
+                elements = {() => (
+                    <React.Fragment>
+                        <div className="main--flex">
+                            <div>
+                                <label htmlFor="courseTitle">Course Title</label>
+                                <input 
+                                    id="courseTitle"
+                                    name="courseTitle"
+                                    type="text"
+                                    value={title}
+                                    onChange={change}
+                                    placeholder="Course Title"
+                                />
+                                
+                                <p>By {`${authUser.firstName} ${authUser.lastName}`}</p>
 
-                    elements = {() => (
-                        <React.Fragment>
-                            <div className="main--flex">
-                                <div>
-                                    <label for="courseTitle">Course Title</label>
-                                    <input id="courseTitle" name="courseTitle" type="text" value="" />
-
-                                    <p>By Joe Smith</p>
-
-                                    <label for="courseDescription">Course Description</label>
-                                    <textarea id="courseDescription" name="courseDescription"></textarea>
-                                </div>
-                                <div>
-                                    <label for="estimatedTime">Estimated Time</label>
-                                    <input id="estimatedTime" name="estimatedTime" type="text" value="" />
-
-                                    <label for="materialsNeeded">Materials Needed</label>
-                                    <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-                                </div>
+                                <label htmlFor="courseDescription">Course Description</label>
+                                <textarea 
+                                    id="courseDescription"
+                                    name="courseDescription"
+                                    value={description}
+                                    onChange={change}
+                                    placeholder="Course Description"/>
                             </div>
-                        </React.Fragment>
-                    )}/>
-            </div>
-            
-        );
+                            <div>
+                                <label htmlFor="estimatedTime">Estimated Time</label>
+                                <input 
+                                    id="estimatedTime"
+                                    name="estimatedTime"
+                                    type="text"
+                                    value={estimatedTime}
+                                    onChange={change}
+                                    placeholder="Estimated Time"/>  
+                                <label htmlFor="materialsNeeded">Materials Needed</label>
+                                <textarea 
+                                    id="materialsNeeded"
+                                    name="materialsNeeded"
+                                    value={materialsNeeded}
+                                    onChange={change}
+                                    placeholder="Materials Needed"/> 
+                            </div>
+                        </div>
+                    </React.Fragment>
+                )}/>
+        </div>
+        
+    );
 
 }
 
